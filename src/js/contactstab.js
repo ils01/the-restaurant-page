7558fa4e1e56a1a8f52dd01789eb14a1fa5a547b
ils01 <theilyashulepov@gmail.com>
2023-12-04 14:53:03 +0300

    contacts tab mobile

--- src/js/contactstab.js
+++ src/js/contactstab.js
@@ -1,10 +1,30 @@
+import { createHTMLElement } from "./createElt";
+import "/src/scss/contacts.scss";
+
 function contacts() {
     const page = document.createElement("div");
-    page.classList.add("page");
-    const name = document.createElement("div");
+    page.classList.add("page", "page-contacts");
+    const name = createHTMLElement("div", ["title"]);
     name.textContent = `Contacts`;
-
     page.appendChild(name);
+
+    const lines = [
+        " All work and no play makes Jack a dull boy All work and no play makes Jack a dull boy ",
+        " Jack a dull boy All work and no play makes Jack a dull boy All work and no play makes ",
+        " and no play makes Jack a dull boy All work and no play makes Jack a dull boy All work ",
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const cont = createHTMLElement("div", ["line-container"]);
+        const text = createHTMLElement(
+            "div",
+            ["line-text", `line-${i + 1}`],
+            lines[i]
+        );
+        cont.appendChild(text);
+        page.appendChild(cont);
+    }
+
     return page;
 }
 
